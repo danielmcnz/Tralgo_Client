a552a52a948d7e4d5139d04b98dc263d85f5b4dd
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-11-11 20:43:42 +1300

    .then() requires you return it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState, Link, BrowserRouter as Route } from "react";
-import './App.css';
-import modules from './modules';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 // function GetStock(stock) {
 //   const [data, setData] = React.useState(null);
@@ -18,21 +16,79 @@ import modules from './modules';
 function App() {
   // const stock = GetStock("twtr");
 
-  const [api, setApi] = React.useState(null);
+  const [getData, setGetData] = useState([]);
+  const [postData, setPostData] = useState([]);
 
-  React.useEffect(() => {
-      fetch("https://localhost:5000/api/user/")
-            .then((res) => {res.json(), console.log(res)})
-            .then((data) => setApi(data))
-            .catch((error) => console.log(error))
-  });
+  useEffect(() => {
+    //This is a GET request
+    fetch("http://localhost:5000/api/user")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        //The Backend is will send back hard coded data
+        setGetData(data.users);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-  // const [currentTab, setCurrentTab] = useState('dashboard');
+  useEffect(() => {
+    //This is a POST request so it requires a "headers", "method", and a "body" that conversts are javscript object into a JSON using the JSON.stringify() method.
+    //For every .then() block that modifies your data you must return it so it can then move on to the next .then() block other wise you dont need to use "return"
+    fetch("http://localhost:5000/api/user/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        //The Backend is set up in a way were it only responds back with the data you sent it like below
+        name: "Taj",
+        email: "Created@gmail.com",
+        password: "password123",
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPostData(data);
+        console.log("POST DATA", data);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{api}</p>
+        <h1>GET DATA</h1>
+        <ul>
+          {getData.map((user) => {
+            return (
+              <li key={Math.random()}>
+                <div>
+                  <p>{user.user}</p>
+                  <p>{user.email}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <h1>POST DATA</h1>
+        <ul>
+          {postData.map((user) => {
+            return (
+              <li key={Math.random()}>
+                <div>
+                  <p>{user.name}</p>
+                  <p>{user.email}</p>
+                  <p>{user.password}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
         {/* <ul>
           { modules.map(module => (
             <li key={module.name} className={currentTab == module.name ? 'active' : ''}>
