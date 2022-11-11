f30bbaa4f2498abc909b5d4aa223e9416f494f03
danielmcnz <danielmcnz@gmail.com>
2022-11-11 19:23:28 +1300

    failing

--- src/App.js
+++ src/App.js
@@ -3,26 +3,36 @@ import './App.css';
 import modules from './modules';
 
 
-function GetStock(stock) {
-  const [data, setData] = React.useState(null);
+// function GetStock(stock) {
+//   const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/" + stock)
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+//   React.useEffect(() => {
+//     fetch("/" + stock)
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
 
-  return data;
-}
+//   return data;
+// }
 
 function App() {
   // const stock = GetStock("twtr");
 
-  const [currentTab, setCurrentTab] = useState('dashboard');
+  const [api, setApi] = React.useState(null);
+
+  React.useEffect(() => {
+      fetch("https://localhost:5000/api/user/")
+            .then((res) => {res.json(), console.log(res)})
+            .then((data) => setApi(data))
+            .catch((error) => console.log(error))
+  });
+
+  // const [currentTab, setCurrentTab] = useState('dashboard');
 
   return (
     <div className="App">
       <header className="App-header">
+        <p>{api}</p>
         {/* <ul>
           { modules.map(module => (
             <li key={module.name} className={currentTab == module.name ? 'active' : ''}>
