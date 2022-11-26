f2a0f82612c78929b42f182823671c94c2f9cf31
danielmcnz <danielmcnz@gmail.com>
2022-11-27 09:54:37 +1300

    Removed files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/scenes/global/sidebar.jsx
+++ src/scenes/global/sidebar.jsx
@@ -3,6 +3,7 @@ import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css'
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom"; 
+import { tokens } from "../../theme";
 
 const Sidebar = () => {
     return (
